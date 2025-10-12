db5e06880ff494e1e50e046514f072da369e10b8
Nikuno Menura <60579848+FOREVEREALIZE@users.noreply.github.com>
2025-10-12 23:48:23 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import ClockDigit from "./ClockDigit.svelte";
+
+    let date = $state([0, 0, 0, 0, 0, 0])
+
+    const update = () => {
+        date = new Date().toLocaleString([], {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false,
+        }).split('').filter(c => "1234567890".split("").includes(c)).map(Number)
+    }
+
+    setInterval(update, 100)
+
+    update()
+</script>
+
+<div class="w-screen">
+    <main class="h-screen flex flex-row items-center justify-center bg-neutral-950 gap-8 px-20">
+        <div class="flex flex-row border-4 border-white/20 w-1/3 gap-4 p-4">
+            <ClockDigit n={date[0]}/>
+            <ClockDigit n={date[1]}/>
+        </div>
+        <div class="flex flex-row border-4 border-white/20 w-1/3 gap-4 p-4">
+            <ClockDigit n={date[2]}/>
+            <ClockDigit n={date[3]}/>
+        </div>
+        <div class="flex flex-row border-4 border-white/20 w-1/3 gap-4 p-4">
+            <ClockDigit n={date[4]}/>
+            <ClockDigit n={date[5]}/>
+        </div>
+    </main>
+</div>
+
+<style>
+</style>
---
+++ src/ClockDigit.svelte
@@ -0,0 +1,105 @@
+<script lang="ts">
+    const {n} = $props()
+
+    const nStr = $derived(n.toString())
+
+    const BL = [90, 180]
+    const BR = [0, 90]
+    const TL = [180, 270]
+    const TR = [270, 0]
+    const LR = [0, 180]
+    const TB = [90, 270]
+    const NO = [135, 135]
+
+    const DIGITS = {
+        1: [
+            BR, LR, BL, NO,
+            TR, BL, TB, NO,
+            NO, TB, TB, NO,
+            NO, TB, TB, NO,
+            BR, TL, TR, BL,
+            TR, LR, LR, TL
+        ],
+        2: [
+            BR, LR, LR, BL,
+            TR, LR, BL, TB,
+            BR, LR, TL, TB,
+            TB, BR, LR, TL,
+            TB, TR, LR, BL,
+            TR, LR, LR, TL
+        ],
+        3: [
+            BR, LR, LR, BL,
+            TR, LR, BL, TB,
+            BR, LR, TL, TB,
+            TR, LR, BL, TB,
+            BR, LR, TL, TB,
+            TR, LR, LR, TL
+        ],
+        4: [
+            BR, BL, BR, BL,
+            TB, TB, TB, TB,
+            TB, TR, TL, TB,
+            TR, LR, BL, TB,
+            NO, NO, TB, TB,
+            NO, NO, TR, TL
+        ],
+        5: [
+            BR, LR, LR, BL,
+            TB, BR, LR, TL,
+            TB, TR, LR, BL,
+            TR, LR, BL, TB,
+            BR, LR, TL, TB,
+            TR, LR, LR, TL
+        ],
+        6: [
+            BR, LR, LR, BL,
+            TB, BR, LR, TL,
+            TB, TR, LR, BL,
+            TB, BR, BL, TB,
+            TB, TR, TL, TB,
+            TR, LR, LR, TL
+        ],
+        7: [
+            BR, LR, LR, BL,
+            TR, LR, BL, TB,
+            NO, NO, TB, TB,
+            NO, NO, TB, TB,
+            NO, NO, TB, TB,
+            NO, NO, TR, TL
+        ],
+        8: [
+            BR, LR, LR, BL,
+            TB, BR, BL, TB,
+            TB, TR, TL, TB,
+            TB, BR, BL, TB,
+            TB, TR, TL, TB,
+            TR, LR, LR, TL
+        ],
+        9: [
+            BR, LR, LR, BL,
+            TB, BR, BL, TB,
+            TB, TR, TL, TB,
+            TR, LR, BL, TB,
+            BR, LR, TL, TB,
+            TR, LR, LR, TL
+        ],
+        0: [
+            BR, LR, LR, BL,
+            TB, BR, BL, TB,
+            TB, TB, TB, TB,
+            TB, TB, TB, TB,
+            TB, TR, TL, TB,
+            TR, LR, LR, TL
+        ],
+    } as { [key: string]: number[][] }
+</script>
+
+<div class="grid grid-cols-4 grid-rows-6 w-1/2 gap-1">
+    {#each Array(4*6) as _, i}
+        <div class="border-2 border-white/10 relative aspect-square *:transition">
+            <div class="w-[calc(1/2*100%+2px)] h-1 bg-red-500 absolute top-0 bottom-0 my-auto right-0 origin-[2px_2px] rounded-l-full" style={`transform: rotate(${DIGITS[nStr][i][0]}deg)`}></div>
+            <div class="w-[calc(1/2*100%+2px)] h-1 bg-red-500 absolute top-0 bottom-0 my-auto right-0 origin-[2px_2px] rounded-l-full" style={`transform: rotate(${DIGITS[nStr][i][1]}deg)`}></div>
+        </div>
+    {/each}
+</div>
