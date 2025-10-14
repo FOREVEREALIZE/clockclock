ca1c59cc3aa6e332db75a90403f1ece76fc44848
Nikuno Menura <60579848+FOREVEREALIZE@users.noreply.github.com>
2025-10-14 23:30:02 +0200

    Make all the positions valid (by rotating the clocks)

--- src/ClockDigit.svelte
+++ src/ClockDigit.svelte
@@ -3,13 +3,13 @@
 
     const nStr = $derived(n.toString())
 
-    const BL = [90, 180]
-    const BR = [0, 90]
-    const TL = [180, 270]
-    const TR = [270, 0]
-    const LR = [0, 180]
-    const TB = [90, 270]
-    const NO = [135, 135]
+    const BL = [-90, 180, 270]
+    const BR = [90, 270, 0]
+    const TL = [0, 180, 270]
+    const TR = [0, 270, 0]
+    const LR = [90, 90, 270]
+    const TB = [0, 90, 270]
+    const NO = [0, 135, 135]
 
     const DIGITS = {
         1: [
@@ -95,11 +95,12 @@
     } as { [key: string]: number[][] }
 </script>
 
-<div class="grid grid-cols-4 grid-rows-6 w-1/2 gap-1">
+<div class="grid grid-cols-4 grid-rows-6 w-1/2 gap-1 **:transition">
     {#each Array(4*6) as _, i}
-        <div class="border-2 border-white/10 relative aspect-square *:transition">
-            <div class="w-[calc(1/2*100%+2px)] h-1 bg-red-500 absolute top-0 bottom-0 my-auto right-0 origin-[2px_2px] rounded-l-full" style={`transform: rotate(${DIGITS[nStr][i][0]}deg)`}></div>
+        <div class="border-2 border-white/10 relative aspect-square" style={`transform: rotate(${DIGITS[nStr][i][0]}deg)`}>
+            <div class="h-1 w-0.5 absolute bg-white/50 left-0 right-0 mx-auto z-20"></div>
             <div class="w-[calc(1/2*100%+2px)] h-1 bg-red-500 absolute top-0 bottom-0 my-auto right-0 origin-[2px_2px] rounded-l-full" style={`transform: rotate(${DIGITS[nStr][i][1]}deg)`}></div>
+            <div class="w-[calc(1/2*100%+2px)] h-1 bg-red-500 absolute top-0 bottom-0 my-auto right-0 origin-[2px_2px] rounded-l-full" style={`transform: rotate(${DIGITS[nStr][i][2]}deg)`}></div>
         </div>
     {/each}
 </div>
